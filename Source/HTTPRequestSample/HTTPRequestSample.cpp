fb36aab484c08ac45074f74ecdd2eddc70a46357
TungNguyen <tungnt.rec@gmail.com>
2024-03-27 02:05:52 +0700

    update

---
+++ Source/HTTPRequestSample/HTTPRequestSample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HTTPRequestSample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HTTPRequestSample, "HTTPRequestSample" );
---
+++ Source/HTTPRequestSample/HTTPRequestSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HTTPRequestSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HTTPREQUESTSAMPLE_API AHTTPRequestSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
